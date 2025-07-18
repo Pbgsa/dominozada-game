4ad5d8e9bf76645958502159628d2ce50532e72d
Pbgsa <pedrobgsa@gmail.com>
2025-07-17 23:53:29 -0300

    feat: create main functional structure board scene for reference

--- godot/dominozada/scripts/board.gd
+++ godot/dominozada/scripts/board.gd
@@ -3,18 +3,17 @@ extends Node2D
 @onready var piece_spawner := $PieceSpawner
 @onready var left_head := $Heads/LeftHead
 @onready var right_head := $Heads/RightHead
+@onready var player_hand: HBoxContainer = $CanvasLayer/PlayerHand
 
 var left_value: int = -1
 var right_value: int = -1
 
 func _ready():
-	# Spawn one piece for demonstration purposes at the center of the board
-	spawn_piece_at(Vector2.ZERO, 0)
+	player_hand.piece_played.connect(_on_piece_played)
+	player_hand.passed_turn.connect(_on_passed_turn)
 
-func spawn_piece_at(pos: Vector2, index: int):
-	var piece_scene = preload("res://scenes/domino_piece_2d.tscn")
-	var piece = piece_scene.instantiate()
-	piece.position = pos
-	piece.set_values(index)
-	piece_spawner.add_child(piece)
-#dasdas
+func _on_piece_played(piece_data: Dictionary):
+	print("Jogador jogou:", piece_data)
+
+func _on_passed_turn():
+	print("Jogador passou a vez")
