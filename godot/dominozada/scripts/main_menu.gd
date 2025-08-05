f2a94d7eaa4130823c6bdc3ba5048514918151c1
artur-boa-viagem <arturbv2003@gmail.com>
2025-08-05 20:15:56 -0300

    :construction: Start main menu implementation

---
+++ godot/dominozada/scripts/main_menu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+
+func _on_botao_modo_classico_pressed() -> void:
+	# IMPORTANTE: Substitua "res://..." pelo caminho real da sua cena.
+	get_tree().change_scene_to_file("res://scenes/board.tscn")
+	pass
+
+
+func _on_botao_modo_burrinho_pressed() -> void:
+	# IMPORTANTE: Substitua "res://..." pelo caminho real da sua cena.
+	#get_tree().change_scene_to_file("res://scenes/gameplay/modo_classico.tscn")
+	pass
+
+
+func _on_botao_modo_gato_com_lebre_pressed() -> void:
+	# IMPORTANTE: Substitua "res://..." pelo caminho real da sua cena.
+	#get_tree().change_scene_to_file("res://scenes/gameplay/modo_classico.tscn")
+	pass
+
+
+func _on_botao_multiplayer_pressed() -> void:
+	# IMPORTANTE: Substitua "res://..." pelo caminho real da sua cena.
+	#get_tree().change_scene_to_file("res://scenes/gameplay/modo_classico.tscn")
+	pass
